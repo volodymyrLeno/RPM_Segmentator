5b54a8199b9a0f412eb71b2888f566e77e0dbee4
volodymyrLeno <leno@ut.ee>
2020-02-12 11:14:13 +1100

    Added general repeats functionality

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,8 @@ public class Main {
         Boolean preprocessing = Boolean.parseBoolean(args[2]);
         Boolean considerMissingValues = Boolean.parseBoolean(args[3]);
 
+        /********** Graph-based approach **********/
+
         List<Event> events = logReader.readCSV(filePath);
 
         if(preprocessing)
@@ -24,11 +26,49 @@ public class Main {
         DirectlyFollowsGraph dfg = new DirectlyFollowsGraph(events);
         dfg.buildGraph();
         dfg.convertIntoDOT();
-        //dfg.getAdjacencyMatrix();
 
         SegmentsDiscoverer disco = new SegmentsDiscoverer();
         Map<Integer, List<Event>> cases = disco.extractSegmentsFromDFG(dfg);
         Utils.writeSegments(filePath.substring(0, filePath.lastIndexOf(".")) + "_segmented.csv", cases);
         System.out.println("Done!");
+
+
+        /********** General repeats mining **********/
+
+        /*
+        List<Event> events = logReader.readCSV(filePath);
+        if(preprocessing)
+            events = Preprocessor.applyPreprocessing(filePath, Utils.eventListToString(events));
+        repeatsMiner.m(Utils.toSequence(events, threshold, considerMissingValues), 1);
+        */
+
+        /********** Some testing *********/
+
+        // Dynamic programming approach \\
+
+        //repeatsDiscoverer.discoverRepeats("GCGAGAGACGCC", 1);
+        //repeatsDiscoverer.discoverRepeats("AXBCDABCD", 1);
+        //repeatsDiscoverer.discoverRepeats("ABCDAXBCD", 1);
+        //repeatsDiscoverer.discoverRepeats("ABDBDABCADC", 1);
+        //repeatsDiscoverer.discoverRepeats("ABCDABCDABCDABCD", 1); // searching for maximal repeat
+        //repeatsDiscoverer.discoverRepeats("ABCABCAB", 1); // overlapping repeats
+
+        //repeatsDiscoverer.discoverRepeats("abcdxaybwabcdybxawabcd", 1);
+        //repeatsDiscoverer.discoverRepeats("abcdxadbcx", 1);
+        //repeatsDiscoverer.discoverRepeats("abcdbcabcbcd", 1);
+
+        // Shift-based approach \\
+
+        //repeatsDiscoverer.discoverRepeats("GCGAGAGACGCC", 3, 1);
+        //repeatsDiscoverer.discoverRepeats("AXBCDABCD", 3, 1); // removal
+        //repeatsDiscoverer.discoverRepeats("ABCDAXBCD", 4, 1); // insertion
+        //repeatsDiscoverer.discoverRepeats("ABDBDABCADC", 3, 1);
+        //repeatsDiscoverer.discoverRepeats("ABCDABCDABCDABCD", 3, 1); // some post processing is required
+        //repeatsDiscoverer.discoverRepeats("ABCABCAB", 3, 1); // dealing with overlapping repeats
+
+        // Find all approximate repeats of a pattern \\
+
+        //repeatsDiscoverer.m("ABDBDABCADC", "ABC", 1);
+        //repeatsDiscoverer.m("GCGAGAGACGCC", "GAGA", 1);
     }
 }
